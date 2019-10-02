624786919e128baa556dff1c517f3b1a2b2e4d3a
Allan Roberto da Silva <37585575+AllanRobert0@users.noreply.github.com>
2019-10-02 16:50:10 -0400

    Update app.js, exports without class

--- src/app.js
+++ src/app.js
@@ -1,20 +1,37 @@
 const express = require("express");
 
-class AppController {
-  constructor() {
-    this.express = express();
+// class AppController {
+//   constructor() {
+//     this.express = express();
 
-    this.middlewares();
-    this.routes();
-  }
+//     this.middlewares();
+//     this.routes();
+//   }
 
-  middlewares() {
-    this.express.use(express.json());
-  }
+//   middlewares() {
+//     this.express.use(express.json());
+//   }
 
-  routes() {
-    this.express.use(require("./routes/routes"));
-  }
-}
+//   routes() {
+//     this.express.use(require("./routes/routes"));
+//   }
+// }
 
-module.exports = new AppController().express;
+// module.exports = new AppController().express;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(require("./routes/routes"));
+
+app.use("/", (req , res) => {
+  res.send("rota '/' ")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("\n\n##Server Running.##\n\n");
+});
+
+module.exports = app;
+
